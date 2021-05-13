66727c193016bac6215d10378636312301873b84
Augusto Pazini <augusto.pazini@hotmail.com>
2021-05-12 21:22:28 -0300

    Implementação do cadastro de pedidos

---
+++ src/app/pedidos/cadastro/cadastro.component.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Customer } from 'src/app/shared/models/customer';
+import { Order } from 'src/app/shared/models/order';
+import { Product } from 'src/app/shared/models/product';
+import { ClienteService } from 'src/app/shared/services/cliente.service';
+import { PedidoService } from 'src/app/shared/services/pedido.service';
+import { ProdutoService } from 'src/app/shared/services/produto.service';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.sass']
+})
+export class CadastroComponent implements OnInit {
+
+  @ViewChild('formulario') form: any;
+
+  clientes: Customer[] = [] as Customer[];
+  produtos: Product[] = [] as Product[];
+  edicao: boolean = false;
+
+  pedido: FormGroup;
+  itens: FormArray;
+
+  constructor(
+    private pedidoService: PedidoService,
+    private clienteService: ClienteService,
+    private produtoService: ProdutoService,
+    private formBuilder: FormBuilder,
+    private modalService: NgbModal
+  ) { }
+
+  ngOnInit(): void {
+    this.pedido = this.formBuilder.group({
+      id: [''],
+      postingDate: [''],
+      deliveryDate: [''],
+      observation: [''],
+      orderLines: this.formBuilder.array([ this.novoItem() ]),
+      customerId: [''],
+    });
+
+    this.popularListaClientes();
+    this.popularListaProdutos();
+  }
+
+  salvar(pedido: Order): void {
+    this.pedidoService.salvar(pedido).then(
+      res => {
+        alert("Pedido cadastrado com sucesso!");
+      },
+      error => {
+        console.error("Erro ao criar cadastro de pedido:\n"
+        + `Status: ${error.error.status}\n` 
+        + `Erro: ${error.error.title} \n`
+        + `${JSON.stringify(error.error, null, 2)}`);
+      }
+    );
+  }
+
+  cancelar(pedido: Order): void {
+    this.pedidoService.cancelar(pedido);
+  }
+
+  open(pedido?: Order, edicao: boolean = false): void {
+    this.edicao = edicao;
+    if(pedido) {
+      this.pedido.setValue(pedido);
+    }
+    this.modalService.open(this.form);
+    this.desabilitarCampos();
+  }
+
+  adicionarItem(): void {
+    this.itens = this.pedido.get('orderLines') as FormArray;
+    this.itens.push(this.novoItem());
+  }
+
+  private novoItem(): FormGroup {
+    return this.formBuilder.group({
+      productId: [''],
+      quantity: [''],
+      unitaryPrice: [''],
+      additionalCosts: ['']
+    });
+  }
+
+  private desabilitarCampos(): void {
+    if(this.pedido.get('id')?.value) {
+      this.pedido.disable();
+    } else {
+      this.pedido.enable();
+    }
+  }
+
+  private popularListaClientes(): void {
+    this.clienteService.getAll().then(clientes => this.clientes = clientes);
+  }
+
+  private popularListaProdutos(): void {
+    this.produtoService.getByCliente({id: "bdf7b85d-b045-4c32-f401-08d914238f47"} as Customer).then(produtos => this.produtos = produtos);
+  }
+
+}
---
+++ src/app/pedidos/pedidos.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuComponent } from '../shared/components/menu/menu.component';
+import { Order } from '../shared/models/order';
+import { PedidoService } from '../shared/services/pedido.service';
+
+@Component({
+  selector: 'app-pedidos',
+  templateUrl: './pedidos.component.html',
+  styleUrls: ['./pedidos.component.sass']
+})
+export class PedidosComponent implements OnInit {
+
+  pedidos: Order[] = [] as Order[];
+  pedidosFiltrados: Order[] = [] as Order[];
+
+  page: number = 1;
+  pageSize: number = 7;
+
+  notFoundText: string = 'Digite ao menos 3 caracteres do nome de produto';
+
+  constructor(
+    private pedidoService: PedidoService,
+  ) { 
+    MenuComponent.toggleExibirMenu.next(true);
+  }
+
+  ngOnInit(): void {
+  }
+
+  filtrarPedidos(termo: string): void {
+    if(termo.length < 3) {
+      this.limparListaPedidos();
+      this.notFoundText = "Digite ao menos 3 caracteres do nome de pedido";
+    } else {
+      if(termo.length == 3) {
+        this.popularListaPedidos().then(() => 
+          this.popularListaPedidosFiltrados(termo)
+        );
+      }
+      this.popularListaPedidosFiltrados(termo);
+      this.notFoundText = `Não foi encontrado nenhum pedido com nome "${termo}"`;
+    }
+  }
+
+  private async popularListaPedidos(): Promise<void> {
+    this.pedidos = await this.pedidoService.getAll();
+  }
+
+  private popularListaPedidosFiltrados(termo: string): void {
+    this.pedidosFiltrados = this.pedidos.filter(pedido => 
+      pedido.postingDate.toLowerCase().includes(termo.toLowerCase())
+    )
+  }
+
+  private limparListaPedidos(): void {
+    this.pedidos = [] as Order[];
+    this.pedidosFiltrados = [] as Order[];
+  }
+
+}
